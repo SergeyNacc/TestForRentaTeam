72c15b73fed21dea3d63ca198f45d64292878c07
SergeyNacc <sergeynaksin@gmail.com>
2022-01-26 14:31:50 +0300

    added Adapter, ViewHolder

--- app/src/main/java/nacc/sergey/testforrentateam/domain/model/User.kt
+++ app/src/main/java/nacc/sergey/testforrentateam/domain/model/User.kt
@@ -7,6 +7,7 @@ data class User(
         val first_name: String,
         val last_name: String,
         val full_name: String,
+        val email: String,
         val avatarUrl: Int,
 
 ) : Serializable
--- app/src/main/java/nacc/sergey/testforrentateam/presentation/list/UsersAdapter.kt
+++ app/src/main/java/nacc/sergey/testforrentateam/presentation/list/UsersAdapter.kt
@@ -1,4 +1,29 @@
 package nacc.sergey.testforrentateam.presentation.list
 
-class UsersAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import nacc.sergey.testforrentateam.databinding.UserItemBinding
+import nacc.sergey.testforrentateam.domain.model.User
+import nacc.sergey.testforrentateam.presentation.list.UsersAdapter.UserViewHolder
+
+class UsersAdapter : ListAdapter<User, UserViewHolder>(UsersDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding = UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = getItem(position)
+        holder.bind(user)
+    }
+
+    class UserViewHolder(val binding: UserItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.userItemName.text = user.full_name
+            binding.userItemEmail.text = user.email
+        }
+    }
 }
